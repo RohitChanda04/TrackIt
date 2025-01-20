8051d28aa45552720e062d0f9cd03a06c61bfe26
Rohit Chanda <rohit.chanda4@gmail.com>
2025-01-19 19:41:32 -0800

    env for port

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
-import express = require('express');
-const cors = require('cors');
-const router = require('../src/routes/JobApplicationRoutes');
-import sequelize from '../src/config/db';
+import express = require("express");
+const cors = require("cors");
+const router = require("../src/routes/JobApplicationRoutes");
+import sequelize from "../src/config/db";
 
 // Initializing the Express app
 const app = express();
@@ -11,24 +11,23 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/applications', router);
+app.use("/applications", router);
 
 // Starting the server on port 3002
-const PORT = 3002;
+const PORT = process.env.PORT;
 
 const startServer = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log("Database connected...");
-        await sequelize.sync();
+  try {
+    await sequelize.authenticate();
+    console.log("Database connected...");
+    await sequelize.sync();
 
-        app.listen(PORT, () => {
-            console.log(`Server is running on http://localhost:${PORT}`);
-        });
-    }
-    catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
 };
 
 startServer();
